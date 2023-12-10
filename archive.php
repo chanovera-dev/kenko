5c59ef4a67ad951a6f9fc8473c616e93b165dd2c
= <=>
2023-12-10 10:21:30 -0600

    archivo de posts

---
+++ archive.php
@@ -0,0 +1,33 @@
+<?php
+get_header();
+
+    echo '
+    <main id="main">
+        <div class="container">
+            <section class="section">';
+                  
+                if ( have_posts() ){           
+                    echo '
+                    <div class="posts">';
+                        
+
+                        while( have_posts() ){            
+                            the_post();  
+                            get_template_part( 'templates/content', 'archive' );    
+                        }
+
+                        echo '
+                        
+                    </div>';     
+                } else {
+                    echo '<p>' . esc_html__('No se encontraron coincidencias', 'kenko') . '</p>';
+                }
+
+                the_post_pagination();
+                
+            echo '
+            </section>
+        </div>
+    </main>';
+    
+get_footer();
