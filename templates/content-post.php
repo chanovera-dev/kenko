38181b1fd9f6c658b0e25c2bf797b3d258be4f32
= <=>
2023-12-10 17:45:46 -0600

    ajustes al single.css para los artículos

--- templates/content-post.php
+++ templates/content-post.php
@@ -49,15 +49,17 @@ echo '
     <div class="container pagination-wrapper">
         <section class="single-post-pagination">
             <div class="previous-post-link">';
-                previous_post_link('
-                    <a href="%link" class="custom-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                        </svg>
-                        <p>Anterior</p>
-                        <p>%title</p>
-                    </a>
-                ');
+            $prev_post = get_previous_post();
+            if ($prev_post) :
+            ?>
+               <a href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>" class="custom-link">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                     <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
+                  </svg>
+                  <p>Anterior</p>
+                  <p><?php echo esc_html($prev_post->post_title); ?></p>
+               </a>
+            <?php endif;
             echo '
             </div>
             <div class="next-post-link">';
