e4a62f4a2f8d4153803e753841d549c00b5a3cd2
= <=>
2023-12-10 10:25:43 -0600

    archivo de posts

--- templates/content-page.php
+++ templates/content-page.php
@@ -4,8 +4,12 @@ echo '
     <div class="container">
         <article class="section">';
             the_title('<h1 class="title">','</h1>');
-            echo '
-            <img src="'; the_post_thumbnail_url( 'full' ); echo '" alt="'.esc_html__('Imagen destacada', 'kenko').'">';
+            if ( has_post_thumbnail() == false ) :
+        
+            else:
+                echo '
+                <img class="thumbnail" src="'; the_post_thumbnail_url( 'full' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">';
+            endif;
             the_content();
         echo '
         </article>
