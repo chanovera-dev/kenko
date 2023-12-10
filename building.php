1342a2287ed5508a399ea1499fcce44b27e44c94
= <=>
2023-12-10 01:40:25 -0600

    template de página

--- building.php
+++ building.php
@@ -1,14 +1,8 @@
 <?php /* Template name: En construcción */
 
+get_header();
+
 echo '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Estamos en construcción</h1>
-</body>
-</html>';
+<h1>Estamos en construcción</h1>';
+
+get_footer();
