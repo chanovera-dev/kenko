7c1bde54827ee64b08f3910af73a6f19ded73566
= <=>
2023-12-10 01:20:03 -0600

    carga de soportes para el sitio

--- functions.php
+++ functions.php
@@ -15,3 +15,50 @@ function load_components_footer(){
 
 }
 add_action( 'get_footer', 'load_components_footer' );
+
+
+
+
+function kenko_theme_support(){ 
+    
+    add_theme_support( 'title-tag' );
+    
+    add_theme_support( 'automatic-feed-links' );
+    
+    add_theme_support( 'custom-logo', array( 
+    'height' => 240,
+    'width' => 240, 
+    'flex-height' => true,
+    ) );
+
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
+    
+    add_theme_support( 'post-formats', array(
+        'aside',
+        'image', 
+        'video',
+        'quote',
+        'link',
+        'gallery',
+        'status',
+        'audio',
+        'chat',
+    ) );
+    
+    add_theme_support( 'customize-selective-refresh-widgets' );
+
+    add_theme_support( 'post-thumbnails', array( 'post' ) ); // Add it for posts
+	set_post_thumbnail_size( 200, 200, true ); // Normal post thumbnails, hard crop mode
+	add_image_size( 'post-image', 600, 200, true ); // Post thumbnails, hard crop mode
+	add_image_size( 'slider-image', 920, 300, true ); // Post thumbnails for slider, hard crop mode
+
+	add_theme_support('custom-background');
+
+} 
+add_action('after_setup_theme', 'kenko_theme_support');
