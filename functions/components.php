81ceb696a89f9d9a5ea4ae3a66e7cd2567941039
= <=>
2023-12-11 14:12:55 -0600

    ajustes al blog

--- functions/components.php
+++ functions/components.php
@@ -8,3 +8,13 @@ function kenko_single_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'kenko_single_styles' );
+
+// Estilos para la página archivo
+function archive_styles() {
+    if ( is_home() or is_page_template('home.php') or is_archive() or is_search() ) {
+        wp_dequeue_style( 'wp-block-library' );
+        wp_enqueue_style( 'blog-styles', get_template_directory_uri() . '/assets/css/blog.css' );
+        wp_enqueue_style( 'pagination-styles', get_template_directory_uri() . '/assets/css/pagination.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'archive_styles' );
