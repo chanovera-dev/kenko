272c613e9b46dec3c1ba3f355efc9b31ed6f7e18
= <=>
2023-12-11 10:16:07 -0600

    ajustes al single.css para los artículos

--- functions/components.php
+++ functions/components.php
@@ -4,6 +4,7 @@
 function kenko_single_styles() {
     if ( is_single() ) {
         wp_enqueue_style( 'single-styles', get_template_directory_uri() . '/assets/css/single.css' );
+        wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'kenko_single_styles' );
