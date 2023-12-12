d60a4557879f6ebb6770cd57299cb3b1feb2f0b0
= <=>
2023-12-11 22:57:02 -0600

    cabecera del blog en php

--- home.php
+++ home.php
@@ -5,20 +5,23 @@ echo '
 <main id="main">
     <div class="container">
         <section class="section">';
-        ?>
-        <?php $categories = get_categories(); ?>
+
+        $categories = get_categories();
+        echo '
         <ul class="categories-list">
-            <li><a class="category-list_item active" href="#!" data-slug="">Todo</a></li>
+            <li><a class="category-list_item active" href="#!" data-slug="">' . esc_html__('Todo', 'kenko') . '</a></li>';
 
-        <?php foreach($categories as $category) : ?>
+        foreach($categories as $category) :
+            echo '
             <li>
-            <a class="category-list_item" href="#!" data-slug="<?= $category->slug; ?>">
-                <?= $category->name; ?>
+            <a class="category-list_item" href="#!" data-slug="' . $category->slug . '">' .
+                $category->name . '
             </a>
-            </li>
-        <?php endforeach; ?>
-        </ul>
-
+            </li>';
+        endforeach;
+        echo '
+        </ul>';
+        ?>
         <?php 
             $projects = new WP_Query([
             'post_type' => 'post',
