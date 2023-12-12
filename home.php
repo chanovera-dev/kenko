b0741e98a2b0f3aa5ffba0c4443f99cd6a226aac
= <=>
2023-12-11 18:58:10 -0600

    ajustes al blog

--- home.php
+++ home.php
@@ -1,33 +1,66 @@
-<?php
+/*<?php
 get_header();
 
-    echo '
-    <main id="main">
-        <div class="container">
-            <section class="section">';
-                  
-                if ( have_posts() ){           
-                    echo '
-                    <div class="posts">';
-                        
+echo '
+<main id="main">
+    <div class="container">
+        <section class="section">';
 
-                        while( have_posts() ){            
-                            the_post();  
-                            get_template_part( 'templates/content', 'archive' );    
-                        }
-
-                        echo '
-                        
-                    </div>';     
-                } else {
-                    echo '<p>' . esc_html__('No se encontraron artículos', 'kenko') . '</p>';
-                }
-
-                the_posts_pagination();
-                
             echo '
-            </section>
-        </div>
-    </main>';
+            <div class="ajax-filters">
+                <form id="ajax-filter">';
+                    
+                    $categories = get_terms(
+                        array(
+                            'taxonomy' => 'category',
+                            'orderby'  => 'name',
+                        )
+                    );
+                    if ($categories) :
+                        echo '
+                        <div>
+                            <span>Select category:</span>';
+                            
+                            foreach ($categories as $category) :
+                                echo '
+                                <label>
+                                    <input type="radio" name="category" value="' . $category->term_id . '">' .
+                                    $category->name . '
+                                </label>';
+                            
+                            endforeach;
+                        echo '
+                        </div>';
+                    
+                    endif;
+                echo '
+                </form>
+            </div>';
+
+    
+                
+            // if ( have_posts() ){           
+            //     echo '
+            //     <div class="posts">';
+                    
+
+            //         while( have_posts() ){            
+            //             the_post();  
+            //             get_template_part( 'templates/content', 'archive' );    
+            //         }
+
+            //         echo '
+                    
+            //     </div>';     
+            // } else {
+            //     echo '<p>' . esc_html__('No se encontraron artículos', 'kenko') . '</p>';
+            // }
+
+            // the_posts_pagination();
+            
+        echo '
+        </section>
+    </div>
+</main>';
     
 get_footer();
