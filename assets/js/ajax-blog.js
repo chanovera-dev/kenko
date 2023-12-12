60553396e630760502b04b5529fa984c658c3741
= <=>
2023-12-11 22:15:37 -0600

    cabecera del blog en php

--- assets/js/ajax-blog.js
+++ assets/js/ajax-blog.js
@@ -1,17 +1,19 @@
-$('.category-list_item').on('click', function() {
-    $('.category-list_item').removeClass('active');
-    $(this).addClass('active');
-  
-    $.ajax({
-      type: 'POST',
-      url: '/wp-admin/admin-ajax.php',
-      dataType: 'html',
-      data: {
-        action: 'filter_projects',
-        category: $(this).data('slug'),
-      },
-      success: function(res) {
-        $('.project-tiles').html(res);
-      }
-    })
-  });
+jQuery(document).ready(function($) {
+    $('.category-list_item').on('click', function() {
+        $('.category-list_item').removeClass('active');
+        $(this).addClass('active');
+
+        $.ajax({
+            type: 'POST',
+            url: '/wp-admin/admin-ajax.php',
+            dataType: 'html',
+            data: {
+                action: 'filter_projects',
+                category: $(this).data('slug'),
+            },
+            success: function(res) {
+                $('.project-tiles').html(res);
+            }
+        });
+    });
+});
