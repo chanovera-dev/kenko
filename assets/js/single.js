2cbcd9ace1845340b0f3ee184df324e87dc62dad
= <=>
2023-12-13 13:45:07 -0600

    ajustes a la galería del single post

---
+++ assets/js/single.js
@@ -0,0 +1,9 @@
+let galleryElements = document.getElementsByClassName('wp-block-galley');
+
+// Verificar si la cantidad de elementos es par
+if (galleryElements.length % 2 === 0) {
+// Agregar la clase 'grid' a todos los elementos
+for (var i = 0; i < galeriaElements.length; i++) {
+    galeriaElements[i].classList.add('grid');
+}
+}
--- functions/components.php
+++ functions/components.php
@@ -5,6 +5,7 @@ function kenko_single_styles() {
     if ( is_single() ) {
         wp_enqueue_style( 'single-styles', get_template_directory_uri() . '/assets/css/single.css' );
         wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' );
+        wp_enqueue_script( 'single-js', get_template_directory_uri() . '/assets/js/single.js', array(), '1.0', true );
     }
 }
 add_action( 'wp_enqueue_scripts', 'kenko_single_styles' );
