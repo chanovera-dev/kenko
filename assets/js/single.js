7ded9dd3e697549272807189ff8d164aee02cdae
= <=>
2023-12-13 13:48:35 -0600

    ajustes a la galería del single post

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,7 +3,7 @@ let galleryElements = document.getElementsByClassName('wp-block-galley');
 // Verificar si la cantidad de elementos es par
 if (galleryElements.length % 2 === 0) {
 // Agregar la clase 'grid' a todos los elementos
-for (var i = 0; i < galeriaElements.length; i++) {
+for (var i = 0; i < galleryElements.length; i++) {
     galleryElements[i].classList.add('grid');
 }
 }
