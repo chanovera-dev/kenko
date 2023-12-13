a0fd47d1e99d7abbcdcea3ce2cbb8c9443da7c1f
= <=>
2023-12-13 13:55:04 -0600

    ajustes a la galería del single post

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,11 @@
-let galleryElements = document.getElementsByClassName('wp-block-gallery');
+document.addEventListener('DOMContentLoaded', function () {
+    let galleryElements = document.getElementsByClassName('wp-block-gallery');
 
-// Verificar si la cantidad de elementos es par
-if (galleryElements.length % 2 === 0) {
-// Agregar la clase 'grid' a todos los elementos
-for (var i = 0; i < galleryElements.length; i++) {
-    galleryElements[i].classList.add('grid');
-}
-}
+    // Verificar si la cantidad de elementos es par
+    if (galleryElements.length % 2 === 0) {
+        // Agregar la clase 'grid' a todos los elementos
+        for (var i = 0; i < galleryElements.length; i++) {
+            galleryElements[i].classList.add('grid');
+        }
+    }
+});
