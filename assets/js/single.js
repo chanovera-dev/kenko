53fa48940d850df72ce98d7f882c4d0041d643fd
= <=>
2023-12-13 14:11:59 -0600

    ajustes a la galería del single post

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Selecciona todos los elementos con la clase 'post' que contienen la clase 'wp-block-gallery'
-    let galleryContainers = document.querySelectorAll('.post .wp-block-gallery');
+    // Selecciona todos los elementos que contienen la clase 'wp-block-gallery'
+    let galleryContainers = document.querySelectorAll('.wp-block-gallery');
 
     // Itera sobre los contenedores de galerías
     galleryContainers.forEach(function (container) {
-        // Selecciona los elementos hijos directos con la clase 'blocks-gallery-item' dentro de cada contenedor
-        let galleryItems = container.querySelectorAll('.post .wp-block-gallery .wp-block-image');
+        // Selecciona los elementos hijos directos con la clase 'wp-block-image' dentro de cada contenedor
+        let galleryItems = container.querySelectorAll('.wp-block-image');
 
         // Verifica si la cantidad de elementos hijos es par
         if (galleryItems.length % 2 === 0) {
-            // Agrega la clase 'grid' a todos los elementos hijos
-            galleryContainers.classList.add('grid');
+            // Agrega la clase 'grid' al contenedor actual
+            container.classList.add('grid');
         }
     });
 });
