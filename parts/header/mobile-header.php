7e3bbc4f1d105d7ade611100e012fa4cc28e4067
= <=>
2023-12-14 15:04:49 -0600

    mobile-header · botón de menú

--- parts/header/mobile-header.php
+++ parts/header/mobile-header.php
@@ -2,6 +2,7 @@
 echo '
 <div class="container mobile-header">
     <section class="section header-content">';
+        include(TEMPLATEPATH . '/parts/header/menu-button.php');
         include(TEMPLATEPATH . '/parts/header/brand.php');
     echo '
     </section>
