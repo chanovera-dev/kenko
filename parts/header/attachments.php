489ea3dab1a5d2ebe3b7858320a4052bba66d076
= <=>
2023-12-15 01:22:45 -0600

    mobile-header · cart

--- parts/header/attachments.php
+++ parts/header/attachments.php
@@ -7,12 +7,17 @@ if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
         do_shortcode('[yith_wcwl_items_count]').'
     </li>';
 } else {}
+if (is_plugin_active('woocommerce/woocommerce.php')) {
     echo '
+    <li>
+        <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
+    </li>
     <li>
         <a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">'.
             esc_html__('Carrito', 'kenko').'
             <div class="wrapper"><span class="parentesis">'.esc_html__('(', 'kenko').'</span><span class="number">'; echo sprintf ( WC()->cart->get_cart_contents_count() );  echo'</span><span class="parentesis">'.esc_html__(')', 'kenko').'</span></div>
         </a>
     </li>';
+} else {}
 echo '
 </ul>';
