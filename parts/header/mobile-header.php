1f683f8f37391f24f4f40ca9d4f1fe3022ad7155
= <=>
2023-12-15 02:06:09 -0600

    mobile-header · cart

--- parts/header/mobile-header.php
+++ parts/header/mobile-header.php
@@ -3,6 +3,13 @@ echo '
 <div class="container mobile-header">
     <section class="section header-content">';
         include(TEMPLATEPATH . '/parts/header/menu-button.php');
+        wp_nav_menu(
+            array(
+                'container' => 'nav', 
+                'container_class' => 'primary', 
+                'theme_location' => 'primary',
+            ) 
+        );
         include(TEMPLATEPATH . '/parts/header/brand.php');
         include(TEMPLATEPATH . '/parts/header/attachments.php');
     echo '
