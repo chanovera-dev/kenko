7d341f5ed24ed8571a6e7197dacb91850b6f665d
= <=>
2023-12-15 01:37:27 -0600

    mobile-header · cart

--- parts/header/attachments.php
+++ parts/header/attachments.php
@@ -12,7 +12,7 @@ if (is_plugin_active('woocommerce/woocommerce.php')) {
     <li class="sign-in-wrapper">
         <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
     </li>
-    <li>
+    <li class="cart-wrapper">
         <a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">'.
             esc_html__('Carrito', 'kenko').'
             <div class="wrapper"><span class="parentesis">'.esc_html__('(', 'kenko').'</span><span class="number">'; echo sprintf ( WC()->cart->get_cart_contents_count() );  echo'</span><span class="parentesis">'.esc_html__(')', 'kenko').'</span></div>
