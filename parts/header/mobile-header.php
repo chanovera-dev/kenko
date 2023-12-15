47fe8579a9d87c6ff65e2ec151117764035ed3c3
= <=>
2023-12-15 00:55:20 -0600

    mobile-header · cart

--- parts/header/mobile-header.php
+++ parts/header/mobile-header.php
@@ -4,6 +4,7 @@ echo '
     <section class="section header-content">';
         include(TEMPLATEPATH . '/parts/header/menu-button.php');
         include(TEMPLATEPATH . '/parts/header/brand.php');
+        include(TEMPLATEPATH . '/parts/header/attachments.php');
     echo '
     </section>
 </div>';
