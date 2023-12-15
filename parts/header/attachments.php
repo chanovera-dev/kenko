a6933cdb606227714dddb1c664400b037a8f4fd6
= <=>
2023-12-15 02:12:26 -0600

    mobile-header · menús directamente sobre la cabecera

--- parts/header/attachments.php
+++ parts/header/attachments.php
@@ -1,23 +1,25 @@
 <?php
 echo '
-<ul class="attachment-list">';
-if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
+<nav>
+    <ul class="attachment-list">';
+    if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
+        echo '
+        <li class="wishlist-wrapper">'.
+            do_shortcode('[yith_wcwl_items_count]').'
+        </li>';
+    } else {}
+    if (is_plugin_active('woocommerce/woocommerce.php')) {
+        echo '
+        <li class="sign-in-wrapper">
+            <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
+        </li>
+        <li class="cart-wrapper">
+            <a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">'.
+                esc_html__('Carrito', 'kenko').'
+                <div class="wrapper"><span class="parentesis">'.esc_html__('(', 'kenko').'</span><span class="number">'; echo sprintf ( WC()->cart->get_cart_contents_count() );  echo'</span><span class="parentesis">'.esc_html__(')', 'kenko').'</span></div>
+            </a>
+        </li>';
+    } else {}
     echo '
-    <li class="wishlist-wrapper">'.
-        do_shortcode('[yith_wcwl_items_count]').'
-    </li>';
-} else {}
-if (is_plugin_active('woocommerce/woocommerce.php')) {
-    echo '
-    <li class="sign-in-wrapper">
-        <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
-    </li>
-    <li class="cart-wrapper">
-        <a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">'.
-            esc_html__('Carrito', 'kenko').'
-            <div class="wrapper"><span class="parentesis">'.esc_html__('(', 'kenko').'</span><span class="number">'; echo sprintf ( WC()->cart->get_cart_contents_count() );  echo'</span><span class="parentesis">'.esc_html__(')', 'kenko').'</span></div>
-        </a>
-    </li>';
-} else {}
-echo '
-</ul>';
+    </ul>
+</nav>';
