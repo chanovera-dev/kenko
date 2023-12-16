f6f24ce13be9bfaaa12f6d0da046de340a9421b6
= <=>
2023-12-16 15:12:07 -0600

    footer content · clases css del menú secundario y del copyright

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -63,7 +63,7 @@ function kenko_theme_custom_breakpoints() {
                     .comment-form :is(.comment-notes, .comment-form-comment, .comment-form-url, .comment-form-cookies-consent, .form-submit){grid-column:1/-1;}
                 
                     /* footer */
-                    #main-footer .footer-content{display:grid; grid-template-columns:auto 1fr; gap:18px 14px;}
+                    #main-footer .footer-content{display:grid; grid-template-columns:auto 1fr; gap:18px 14px; align-items:baseline;}
                         /* footer parrafo */
                         #main-footer p{font-size:14px; padding:3px; grid-column:2/3;}
                         /* menú secundario */
