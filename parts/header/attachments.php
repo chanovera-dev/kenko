733cc610d4773147243b83ec5a7f7afee9331c03
= <=>
2023-12-17 13:29:10 -0600

    menu mobile · php · css · js · menú abierto

--- parts/header/attachments.php
+++ parts/header/attachments.php
@@ -2,6 +2,12 @@
 echo '
 <nav class="attachment-nav">
     <ul class="attachment-list">';
+    if (is_plugin_active('woocommerce/woocommerce.php')) {
+        echo '
+        <li class="sign-in-wrapper">
+            <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
+        </li>';
+    } else {}
     if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
         echo '
         <li class="wishlist-wrapper">'.
@@ -10,9 +16,6 @@ echo '
     } else {}
     if (is_plugin_active('woocommerce/woocommerce.php')) {
         echo '
-        <li class="sign-in-wrapper">
-            <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
-        </li>
         <li class="cart-wrapper">
             <a class="counter cart-customlocation" href="'; echo esc_url(wc_get_cart_url()); echo '">'.
                 esc_html__('Carrito', 'kenko').'
--- parts/header/menu-mobile.php
+++ parts/header/menu-mobile.php
@@ -19,16 +19,16 @@ echo '
     echo '
     <nav class="attachment-nav">
         <ul class="attachment-list">';
-        if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
-            echo '
-            <li class="wishlist-wrapper">'.
-                do_shortcode('[yith_wcwl_items_count]').'
-            </li>';
-        } else {}
         if (is_plugin_active('woocommerce/woocommerce.php')) {
             echo '
             <li class="sign-in-wrapper">
                 <a href="' . esc_url(home_url('/mi-cuenta')) . '">' . esc_html__('Acceso', 'kenko') . '</a>
+            </li>';
+        } else {}
+        if (is_plugin_active('yith-woocommerce-wishlist/init.php')) {
+            echo '
+            <li class="wishlist-wrapper">'.
+                do_shortcode('[yith_wcwl_items_count]').'
             </li>';
         } else {}
         echo '
