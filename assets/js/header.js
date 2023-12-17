187f0407ee095696ed453065715ff4e2078c62f0
= <=>
2023-12-17 11:44:57 -0600

    menu mobile · php · css · js · botón del menú

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,31 +1,32 @@
 /* arreglo del botón del menú y del menú */
 function myFunction(x) {
-    // let nav = document.getElementById("menu-mobile--wrapper");
+    let navMobile = document.querySelector('.menu-mobile');
     // let body = document.getElementById("body");
     // let header = document.getElementById("responsive-header");
-    // let main = document.getElementById("main");
+    let main = document.getElementById('main');
     // let footer = document.getElementById("main-footer");
-    x.classList.toggle("active");
+    x.classList.toggle('active');
 
-    // if (!nav.classList.contains("active")) {
-    //     nav.classList.add("active");
+    if (!navMobile.classList.contains('open')) {
+        navMobile.classList.add('open');
     //     body.classList.add("menu-active");
     //     header.classList.add("menu-active");
     //     main.classList.add("menu-active");
     //     footer.classList.add("menu-active");
-    // } else {
-    //     nav.classList.remove("active");
+    } else {
+        navMobile.classList.add('close');
     //     body.classList.remove("menu-active");
     //     header.classList.add("menu-inactive");
     //     main.classList.add("menu-inactive");
     //     footer.classList.add("menu-inactive");
-    //     setTimeout(function() {
+         setTimeout(function() {
+            navMobile.classList.remove('open');
     //         header.classList.remove("menu-inactive");
     //         header.classList.remove("menu-active");
     //         main.classList.remove("menu-inactive");
     //         main.classList.remove("menu-active");
     //         footer.classList.remove("menu-inactive");
     //         footer.classList.remove("menu-active");
-    //     }, 300);
-    // }
+         }, 300);
+     }
 }
