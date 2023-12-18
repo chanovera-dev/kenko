3c4d22fde8591d6e8f6c9a863817933c54862b77
= <=>
2023-12-18 08:49:15 -0600

    agregada la fuente maison neue

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php /* Template name: Contacto */
+
+get_header();
+
+echo '
+<main id="main">
+    <div class="container contact-page--wrapper">
+        <section class="section contact-page">
+            
+        </section>
+</main>';
+
+get_footer();
