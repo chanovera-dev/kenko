2b452b4902feb1cae85564cfc836eb7bd622f7de
= <=>
2023-12-18 11:13:08 -0600

    contact

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@ echo '
     <div class="container contact-page--wrapper">
         <section class="section contact-page">
             <h1 class="title-section">' . esc_html('Nos encantaría conocer tu opinión.', 'kenko') . '</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0889968287265!2d-96.1047354221305!3d19.103751282106604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c34030df283fa1%3A0xc5a82fdae0720c9a!2sDise%C3%B1o%20Gr%C3%A1fico%20y%20Desarrollo%20Web%20InGenio%20Alfalfa%20%2F%2F%20PeraManzana!5e0!3m2!1ses-419!2smx!4v1702919470034!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
 </main>';
 
