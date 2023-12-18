56001a27b34156ce9e408ed7652f8835425d5968
= <=>
2023-12-18 00:35:28 -0600

    error 404

--- 404.php
+++ 404.php
@@ -7,6 +7,8 @@ echo '
     <div class="container">
         <section class="section not-found">
             <div class="not-found--icon"></div>
+            <h2>' . esc_html__('Página no encontrada', 'kenko') . '</h2>
+            <p>' . esc_html__('Parece que no se encontró nada en esta ubicación. Haz clic en el enlace de abajo para regresar a la página de inicio.', 'kenko') . '</p>
         </section>
     </div>
 </main>';
