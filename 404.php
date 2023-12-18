265f70ec70834e4e988c78ff61a8a9662517ca25
= <=>
2023-12-18 00:44:58 -0600

    error 404

--- 404.php
+++ 404.php
@@ -9,7 +9,12 @@ echo '
             <div class="not-found--icon"></div>
             <h2>' . esc_html__('Página no encontrada', 'kenko') . '</h2>
             <p>' . esc_html__('Parece que no se encontró nada en esta ubicación. Haz clic en el enlace de abajo para regresar a la página de inicio.', 'kenko') . '</p>
-            <a href="' . site_url() . '">' . esc_html__('Inicio', 'kenko') . '</a>
+            <a href="' . site_url() . '">' . '
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                </svg>' .
+                esc_html__('Inicio', 'kenko') . '
+            </a>
         </section>
     </div>
 </main>';
