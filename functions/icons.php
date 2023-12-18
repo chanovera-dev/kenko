fb9d8a955cf6099ddcd3c1c1de3a8f866b20abe4
= <=>
2023-12-18 09:42:33 -0600

    agregada la fuente nm font

--- functions/icons.php
+++ functions/icons.php
@@ -4,6 +4,15 @@ function kenko_theme_custom_icons() {
     ?>
         <style>          
             /* iconos de redes sociales */
+            @font-face {
+            font-family: 'nm-font';
+            src: url(<?php echo get_template_directory_uri(); ?>/assets/fonts/nm-font.ttf?32v7kg) format('truetype'),
+                 url(<?php echo get_template_directory_uri(); ?>/assets/fonts/nm-font.woff?32v7kg) format('woff'),
+                 url('<?php echo get_template_directory_uri(); ?>/assets/fonts/nm-font.svg?32v7kg#nm-font') format('svg');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap
+}
 
             /* blancos */
             #main-footer .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-white.svg');}
