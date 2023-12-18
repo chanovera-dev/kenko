cb4ca2a79f482ff78bf3404c786a172abe0545c5
= <=>
2023-12-17 18:41:52 -0600

    scroll down · scroll up

--- assets/js/header.js
+++ assets/js/header.js
@@ -87,7 +87,7 @@ const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", (event) => {
   const currentScroll = window.pageYOffset;
   if (currentScroll <= 0) {
     body.classList.remove(scrollUp);
@@ -98,12 +98,12 @@ window.addEventListener("scroll", () => {
     // down
     body.classList.remove(scrollUp);
     body.classList.add(scrollDown);
-    myFunction(x);
+    myFunction(event.target); 
   } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
     // up
     body.classList.remove(scrollDown);
     body.classList.add(scrollUp);
-    myFunction(x);
+    myFunction(event.target); 
   }
   lastScroll = currentScroll;
 });
