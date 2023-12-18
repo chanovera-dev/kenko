557605f9ecf8d76251b21f446858d51bb128e284
= <=>
2023-12-18 09:46:36 -0600

    agregada la fuente nm font · css

--- parts/sidebars/sidebar-post.php
+++ parts/sidebars/sidebar-post.php
@@ -5,10 +5,7 @@ echo '
         <div class="tags">' . get_the_tag_list() . '</div>
         <p class="share">
             <a href="https://www.facebook.com/sharer/sharer.php?u='; the_permalink(); echo '" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="10" fill="currentColor" viewBox="0 0 320 512">
-                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
-                    <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/>
-                </svg>
+                <i class="nm-font nm-font-facebook"></i>
             </a>
             <a href="https://twitter.com/intent/tweet?text='; the_title(); echo '&url='; the_permalink(); echo '&hashtags='; bloginfo( 'title' ); echo '" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
