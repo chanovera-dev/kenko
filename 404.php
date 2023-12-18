ebea34a6a8f8a9a0cc702b7072754d9b88e372d4
= <=>
2023-12-18 00:40:30 -0600

    error 404

--- 404.php
+++ 404.php
@@ -9,6 +9,7 @@ echo '
             <div class="not-found--icon"></div>
             <h2>' . esc_html__('Página no encontrada', 'kenko') . '</h2>
             <p>' . esc_html__('Parece que no se encontró nada en esta ubicación. Haz clic en el enlace de abajo para regresar a la página de inicio.', 'kenko') . '</p>
+            <a href="' . site_url() . '">' . esc_html__('Inicio', 'kenko') . '</a>
         </section>
     </div>
 </main>';
