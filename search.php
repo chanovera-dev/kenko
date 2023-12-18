043fb67254842048fcfd1a42badafee750b908e5
= <=>
2023-12-17 20:48:48 -0600

    search · título

---
+++ search.php
@@ -0,0 +1,35 @@
+<?php
+get_header();
+
+echo '
+<main id="main">
+    <div class="container">
+        <section class="section">';
+
+        echo '<h1 class="title-section">' . esc_html__('Resultados de búsqueda para: ', 'kenko') . the_search_query() . '</h1>';
+                
+            if ( have_posts() ){           
+                echo '
+                <div class="posts">';
+                    
+
+                    while( have_posts() ){            
+                        the_post();  
+                        get_template_part( 'templates/content', 'archive' );    
+                    }
+
+                    echo '
+                    
+                </div>';     
+            } else {
+                echo '<p>' . esc_html__('No se encontraron coincidencias', 'kenko') . '</p>';
+            }
+
+            the_posts_pagination();
+            
+        echo '
+        </section>
+    </div>
+</main>';
+    
+get_footer();
