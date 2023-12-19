356793ad85217da376eb0d6170cf4c592170a8e9
= <=>
2023-12-19 15:57:17 -0600

    panel de carrito

--- assets/js/header.js
+++ assets/js/header.js
@@ -150,9 +150,9 @@ let abrirCarrito = document.getElementById('open-cart-panel');
 let cerrarCarrito = document.getElementById('close-cart-panel');
 
 abrirCarrito.addEventListener("click", function() {
-    panelCarrito.style.left = "0";
+    panelCarrito.classList.add('open');
 });
 
 cerrarCarrito.addEventListener("click", function() {
-    panelCarrito.style.left = null;
+    panelCarrito.classList.remove('open');
 });
