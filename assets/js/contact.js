10136b7dafa7836a7336d9d68207f15c1efcd55c
= <=>
2023-12-19 12:40:14 -0600

    dar formato al número de contacto

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,23 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtén el elemento con el ID "number-contact"
-    var numeroContacto = document.getElementById('number-contact');
-
-    // Obtén el contenido actual del elemento
-    var numeroCompleto = numeroContacto.textContent;
-
-    // Aplica el formato deseado
-    var numeroFormateado = formatoNumero(numeroCompleto);
-
-    // Actualiza el contenido del elemento con el número formateado
+    let numeroContacto = document.getElementById('number-contact');
+    let numeroCompleto = numeroContacto.textContent;
+    let numeroFormateado = formatoNumero(numeroCompleto);
     numeroContacto.textContent = numeroFormateado;
   });
 
   function formatoNumero(numero) {
-    // Inserta un punto después del tercer número
     numero = numero.slice(0, 3) + '.' + numero.slice(3);
-
-    // Inserta otro punto después del sexto número
     numero = numero.slice(0, 7) + '.' + numero.slice(7);
-
     return numero;
   }
