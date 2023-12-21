94c523edb67ccc6657f3cefe47687d66868c5968
= <=>
2023-12-21 14:45:12 -0600

    single product

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -82,8 +82,8 @@ function kenko_theme_custom_breakpoints() {
                 /* single post */
                     /* imágenes */
                         /* galería */
-                        #main .container .section .post .wp-block-gallery{display:flex; grid-template-columns:1fr 1fr;}
-                        #main .container .section .post .wp-block-gallery.grid{display:grid; grid-template-columns:1fr 1fr;}
+                        .single-post #main .container .section .post .wp-block-gallery{display:flex; grid-template-columns:1fr 1fr;}
+                        .single-post #main .container .section .post .wp-block-gallery.grid{display:grid; grid-template-columns:1fr 1fr;}
                         .wp-block-gallery.has-nested-images.columns-default figure.wp-block-image:not(#individual-image),
                         .wp-block-gallery.has-nested-images.columns-default figure.wp-block-image:not(#individual-image):first-child:nth-last-child(2), 
                         .wp-block-gallery.has-nested-images.columns-default figure.wp-block-image:not(#individual-image):first-child:nth-last-child(2)~figure.wp-block-image:not(#individual-image){width:calc(33.33% - var(--wp--style--unstable-gallery-gap, 16px)*.66667)!important;}
@@ -91,7 +91,7 @@ function kenko_theme_custom_breakpoints() {
                         .wp-block-gallery.grid.has-nested-images.columns-default figure.wp-block-image:not(#individual-image):first-child:nth-last-child(2), 
                         .wp-block-gallery.grid.has-nested-images.columns-default figure.wp-block-image:not(#individual-image):first-child:nth-last-child(2)~figure.wp-block-image:not(#individual-image){width:100%!important;}
                     /* paginación */
-                    #main .container .single-post-pagination :is(.left, .right) :is(.previous-post-link, .next-post-link){height:42.58px;}
+                    .single-post #main .container .single-post-pagination :is(.left, .right) :is(.previous-post-link, .next-post-link){height:42.58px;}
                     /* formulario */
                     .comment-form{display:grid; grid-template-columns:1fr 1fr; gap:0 30px;}
                     .comment-form :is(.comment-notes, .comment-form-comment, .comment-form-url, .comment-form-cookies-consent, .form-submit){grid-column:1/-1;}
