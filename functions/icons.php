833e6515a519d6649980a999dcc2f83841f00734
= <=>
2023-12-21 22:23:49 -0600

    calificación mostrada con estrellas

--- functions/icons.php
+++ functions/icons.php
@@ -667,19 +667,8 @@ function kenko_theme_custom_icons() {
             }
 
 
-            /* blancos */
-            #main-footer .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-white.svg');}
-            #main-footer .social .menu li a[href*="twitter"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/twitter-white.svg');}
-            #main-footer .social .menu li a[href*="youtube"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/youtube-white.svg');}
-            #main-footer .social .menu li a[href*="instagram"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-white.svg');}
-            #main-footer .social .menu li a[href*="sites"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/google-white.svg');}
-
-            /* oscuros */
-            #main-header .mobile-header .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook-dark.svg');}
-            #main-header .mobile-header .social .menu li a[href*="twitter"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/twitter-dark.svg');}
-            #main-header .mobile-header .social .menu li a[href*="youtube"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/youtube-dark.svg');}
-            #main-header .mobile-header .social .menu li a[href*="instagram"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/instagram-dark.svg');}
-            #main-header .mobile-header .social .menu li a[href*="sites"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/google-dark.svg');}
+            /* calificación con estrellas */
+            .comment-form-rating .stars.selected span a.star-1.active{color:#dc9814;}
     <?php
 }
 add_action('wp_head', 'kenko_theme_custom_icons');
