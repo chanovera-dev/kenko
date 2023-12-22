01740d0028c10a0fc1cdb14f502be9c36056845b
= <=>
2023-12-21 22:41:36 -0600

    calificación mostrada con estrellas

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -8,33 +8,33 @@ var classes = commentRating.classList;
 for (var i = 0; i < classes.length; i++) {
   switch (classes[i]) {
     case 'star-5 active':
-      document.querySelector('.star-5').style.color = '#dc9814';
-      document.querySelector('.star-4').style.color = '#dc9814';
-      document.querySelector('.star-3').style.color = '#dc9814';
-      document.querySelector('.star-2').style.color = '#dc9814';
-      document.querySelector('.star-1').style.color = '#dc9814';
+      document.querySelector('.star-5').style.color = '#dc9814!important';
+      document.querySelector('.star-4').style.color = '#dc9814!important';
+      document.querySelector('.star-3').style.color = '#dc9814!important';
+      document.querySelector('.star-2').style.color = '#dc9814!important';
+      document.querySelector('.star-1').style.color = '#dc9814!important';
       break;
 
     case 'star-4 active':
-      document.querySelector('.star-4').style.color = '#dc9814';
-      document.querySelector('.star-3').style.color = '#dc9814';
-      document.querySelector('.star-2').style.color = '#dc9814';
-      document.querySelector('.star-1').style.color = '#dc9814';
+      document.querySelector('.star-4').style.color = '#dc9814!important';
+      document.querySelector('.star-3').style.color = '#dc9814!important';
+      document.querySelector('.star-2').style.color = '#dc9814!important';
+      document.querySelector('.star-1').style.color = '#dc9814!important';
       break;
 
     case 'star-3 active':
-      document.querySelector('.star-3').style.color = '#dc9814';
-      document.querySelector('.star-2').style.color = '#dc9814';
-      document.querySelector('.star-1').style.color = '#dc9814';
+      document.querySelector('.star-3').style.color = '#dc9814!important';
+      document.querySelector('.star-2').style.color = '#dc9814!important';
+      document.querySelector('.star-1').style.color = '#dc9814!important';
       break;
 
     case 'star-2 active':
-    document.querySelector('.star-2').style.color = '#dc9814';
-    document.querySelector('.star-1').style.color = '#dc9814';
+    document.querySelector('.star-2').style.color = '#dc9814!important';
+    document.querySelector('.star-1').style.color = '#dc9814!important';
     break;
 
     case 'star-1 active':
-      document.querySelector('.star-1').style.color = '#dc9814';
+      document.querySelector('.star-1').style.color = '#dc9814!important';
       break;
 
     default:
