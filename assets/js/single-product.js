6bf061ddfe6113f3a0ea2feb4f8a92cff048c5f0
= <=>
2023-12-21 22:53:34 -0600

    calificación mostrada con estrellas

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -4,40 +4,18 @@ var commentRating = document.querySelector('.comment-form-rating .stars span');
 // Obtén la lista de clases del elemento
 var classes = commentRating.classList;
 
+// Quita el estilo de color de todas las estrellas
+document.querySelectorAll('.stars span').forEach(function(star) {
+  star.style.color = '';
+});
+
 // Itera a través de las clases para encontrar la que tiene la propiedad active
 for (var i = 0; i < classes.length; i++) {
-  switch (classes[i]) {
-    case 'star-5 active':
-      document.querySelector('.star-5').style.color = '#dc9814!important';
-      document.querySelector('.star-4').style.color = '#dc9814!important';
-      document.querySelector('.star-3').style.color = '#dc9814!important';
-      document.querySelector('.star-2').style.color = '#dc9814!important';
-      document.querySelector('.star-1').style.color = '#dc9814!important';
-      break;
-
-    case 'star-4 active':
-      document.querySelector('.star-4').style.color = '#dc9814!important';
-      document.querySelector('.star-3').style.color = '#dc9814!important';
-      document.querySelector('.star-2').style.color = '#dc9814!important';
-      document.querySelector('.star-1').style.color = '#dc9814!important';
-      break;
-
-    case 'star-3 active':
-      document.querySelector('.star-3').style.color = '#dc9814!important';
-      document.querySelector('.star-2').style.color = '#dc9814!important';
-      document.querySelector('.star-1').style.color = '#dc9814!important';
-      break;
-
-    case 'star-2 active':
-    document.querySelector('.star-2').style.color = '#dc9814!important';
-    document.querySelector('.star-1').style.color = '#dc9814!important';
-    break;
-
-    case 'star-1 active':
-      document.querySelector('.star-1').style.color = '#dc9814!important';
-      break;
-
-    default:
-      break;
+  if (classes[i].includes('active')) {
+    // Encuentra la clasificación específica y establece el color
+    var ratingClass = classes[i].replace('active', '').trim();
+    document.querySelectorAll('.' + ratingClass).forEach(function(star) {
+      star.style.color = '#dc9814';
+    });
   }
 }
