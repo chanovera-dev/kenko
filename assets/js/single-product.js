4213656bdbbf1a3c3aea21bd7ef9db27a8df91b4
= <=>
2023-12-21 22:56:19 -0600

    calificación mostrada con estrellas

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -1,21 +1,12 @@
-// Obtén el elemento con la clase .comment-form-rating
-var commentRating = document.querySelector('.comment-form-rating .stars span');
+// Selecciona todas las estrellas
+var stars = document.querySelectorAll('.comment-form-rating .stars span');
 
-// Obtén la lista de clases del elemento
-var classes = commentRating.classList;
-
-// Quita el estilo de color de todas las estrellas
-document.querySelectorAll('.stars span').forEach(function(star) {
-  star.style.color = '';
-});
-
-// Itera a través de las clases para encontrar la que tiene la propiedad active
-for (var i = 0; i < classes.length; i++) {
-  if (classes[i].includes('active')) {
-    // Encuentra la clasificación específica y establece el color
-    var ratingClass = classes[i].replace('active', '').trim();
-    document.querySelectorAll('.' + ratingClass).forEach(function(star) {
-      star.style.color = '#dc9814';
+// Asigna un evento de clic a cada estrella
+stars.forEach(function(star, index) {
+  star.addEventListener('click', function() {
+    // Restablece el color de todas las estrellas
+    stars.forEach(function(s, i) {
+      s.style.color = i <= index ? '#dc9814' : ''; // Establece el color solo para las estrellas hasta la calificación actual
     });
-  }
-}
+  });
+});
