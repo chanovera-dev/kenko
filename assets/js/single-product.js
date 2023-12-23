e5370b73249f2f22951d351c00d996587169e52b
= <=>
2023-12-23 00:33:51 -0600

    precio en single product

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -1,14 +1,18 @@
 // cambia el icono del zoom
-let zoomIcon = document.querySelector('.woocommerce-product-gallery__trigger');
-
-  if (zoomIcon) {
-    // Crea un elemento 'i' con las clases 'nm-font nm-font-plus'
-    var iconElement = document.createElement('i');
-    iconElement.className = 'nm-font nm-font-plus';
-
-    // Elimina el contenido actual del div
-    zoomIcon.innerHTML = '';
-
-    // Agrega el nuevo elemento 'i' al div
-    zoomIcon.appendChild(iconElement);
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    // cambia el icono del zoom
+    let zoomIcon = document.querySelector('.woocommerce-product-gallery__trigger');
+  
+    if (zoomIcon) {
+      // Crea un elemento 'i' con las clases 'nm-font nm-font-plus'
+      let iconElement = document.createElement('i');
+      iconElement.className = 'nm-font nm-font-plus';
+  
+      // Elimina el contenido actual del div
+      zoomIcon.innerHTML = '';
+  
+      // Agrega el nuevo elemento 'i' al div
+      zoomIcon.appendChild(iconElement);
+    }
+  });
+  
