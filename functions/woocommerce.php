a173cbb48746d7dfa14ad6d7997ccf8f171e5de7
= <=>
2023-12-22 23:45:09 -0600

    valoraciones en single product

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -48,7 +48,7 @@ require_once(get_template_directory() . '/functions/woocommerce/woocommerce-comp
 remove_action ('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
 
 // Quita el contador de valoraciones en el summary de un solo producto
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+
 
 // Agregar contenido HTML antes del gancho woocommerce_output_content_wrapper
 function my_custom_content_before_wrapper() {
