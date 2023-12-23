024e9017cd24997e7cbd508bc9fe577b379d9c5a
= <=>
2023-12-23 00:09:55 -0600

    valoraciones en single product

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -151,3 +151,17 @@ add_filter( 'woocommerce_product_reviews_tab_title', 'remove_reviews_tab_heading
 function remove_reviews_tab_heading( $title ) {
     return sprintf( __( 'Valoraciones <span>%d</span>', 'woocommerce' ), get_comments_number() );
 }
+
+
+
+// mostrar el porcentaje de descuento
+add_filter( 'woocommerce_get_price_html', 'change_displayed_sale_price_html', 10, 2 ); 
+function change_displayed_sale_price_html( $price, $product ) { 
+	if( $product->is_on_sale() && ! is_admin() && ! $product->is_type('variable')) { 
+		$regular_price = (float) $product->get_regular_price(); 
+		$sale_price = (float) $product->get_price(); 
+		$saving_price = wc_price( $regular_price - $sale_price ); 
+		$precision = 1; 
+		$saving_percentage = round( 100 - ( $sale_price / $regular_price * 100 ), 1 ) . '%'; $price .= sprintf( __('<br><small><span class="snippet-dto-ahorro"></span> <span class="snippet-dto-precio">%s</span> <span class="snippet-dto-porcentaje">(%s)</span></small>', 'woocommerce' ), 
+		$saving_price, $saving_percentage ); } return $price; 
+	}
