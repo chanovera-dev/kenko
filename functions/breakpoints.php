13d9b6e4ac1a01ba6e2151532b3fb11bd8021bf0
= <=>
2023-12-24 02:04:24 -0600

    resumen · single product · css

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -138,6 +138,8 @@ function kenko_theme_custom_breakpoints() {
 
                 /* single product */
                 #main .container.product-wrapper .section .product{display:grid; grid-template-columns:58.3333% 1fr;}
+                    /* resumen */
+                    .summary{max-width:340px; padding:44px 0 0;}
                     /* pestañas */
                     .tabs.wc-tabs{display:flex; justify-content:center; gap:31px;}
                     .tabs.wc-tabs li{height:33px;}
