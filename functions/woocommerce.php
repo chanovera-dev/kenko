e603d3ac8c5e1e907b53e15b5fa7ed0bddd6e155
= <=>
2023-12-26 22:31:20 -0600

    quita la valoración del loop

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -92,4 +92,4 @@ remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_pro
 remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
 
 // desactivar la calificación
-//remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 15);
+remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
