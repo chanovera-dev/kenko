6e103bd26d49899cfb510c6bbe3cccebae09f208
= <=>
2023-12-26 23:26:20 -0600

    agregados estilos para shop

--- functions/woocommerce/woocommerce-components.php
+++ functions/woocommerce/woocommerce-components.php
@@ -19,3 +19,17 @@ function template_redirect_action() {
         remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
     }
 }
+
+
+
+// Estilos para los archivos de la tienda
+function shop_styles() {
+    if ( is_shop() || is_product_category() || is_tax(get_object_taxonomies( 'product' )) ) {
+        wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/woocommerce/shop.css' );
+        /* listas */
+        wp_enqueue_style( 'lists-styles', get_template_directory_uri() . '/assets/css/woocommerce/lists.css' );
+        /* estilos css para la paginación */
+        wp_enqueue_style( 'pagination-styles', get_template_directory_uri() . '/assets/css/pagination.css' ); 
+    }
+}
+add_action( 'wp_enqueue_scripts', 'shop_styles' );
