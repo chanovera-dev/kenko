2a7a1254cad8613e1060db442656412ca84fa640
= <=>
2023-12-29 09:01:08 -0600

    agregado el template de testing para probar el filtrado de woocommerce

---
+++ testing.php
@@ -0,0 +1,18 @@
+<?php /* template name: Testing */
+get_header();
+
+echo '
+<main id="main">
+    <div class="container">
+        <section class="section sections-filters">
+        
+        </section>
+    </div>
+    <div class="container">
+        <section class="section sections-products">
+        
+        </section>
+    </div>
+</main>';
+
+get_footer();
