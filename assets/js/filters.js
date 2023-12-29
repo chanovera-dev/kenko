d139719e9c84fa9085fcfa230c05cb5f5c067018
= <=>
2023-12-29 09:13:33 -0600

    agregado el template de testing para probar el filtrado de woocommerce

---
+++ assets/js/filters.js
@@ -0,0 +1,47 @@
+$('.filter-link').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('activeFilter');
+
+    editFilterInputs($('#filters-' + $(this).data('type')), $(this).data('id'));
+    filterProducts();
+  });
+
+
+
+  function editFilterInputs(inputField, value) {
+    const currentFilters = inputField.val().split(',');
+    const newFilter = value.toString();
+  
+    if (currentFilters.includes(newFilter)) {
+      const i = currentFilters.indexOf(newFilter);
+      currentFilters.splice(i, 1);
+      inputField.val(currentFilters);
+    } else {
+      inputField.val(inputField.val() + ',' + newFilter);
+    }
+  }
+
+
+
+  function filterProducts() {
+    const catIds = $('#filters-category').val().split(',');
+    const creatorIds = $('#filters-creators').val().split(',');
+    
+    $.ajax({
+      type: 'POST',
+      url: '/wp-admin/admin-ajax.php',
+      dataType: 'json',
+      data: {
+        action: 'filter_products',
+        catIds,
+        creatorIds,
+      },
+      success: function(res) {
+        $('#result-count').html(res.total);
+        $('#main-product-list').html(res.html);
+      },
+      error: function(err) {
+        console.error(err);
+      }
+    })
+  }
--- testing.php
+++ testing.php
@@ -5,12 +5,45 @@ echo '
 <main id="main">
     <div class="container">
         <section class="section sections-filters">
-        
+            <div class="filter--sidebar">
+                <input type="hidden" id="filters-category" />
+                <input type="hidden" id="filters-creators" />';
+            
+                // or use include_once('parts/filter-pricerange.php');
+                get_template_part('parts/filter','pricerange');
+            
+                get_template_part('parts/filter','categories');
+                
+                get_template_part('parts/filter','creators');
+            echo '
+            </div>
         </section>
     </div>
     <div class="container">
-        <section class="section sections-products">
-        
+        <section class="section sections-products">';
+
+            $get_creators = new WP_Query([
+            'post_type' => 'creators',
+            'posts_per_page' => -1,
+            ]);
+            
+            if ($get_creators->have_posts()): ?>
+            <h4 class="filter-title"><?= _e('Creators','kenko'); ?></h4>
+            <ul class="list-filters">
+                <?php while ($get_creators->have_posts()):$get_creators->the_post(); ?>
+                
+                <li>
+                    <a href="javascript:;" class="filter-link" data-type="creators" data-id="<?= get_the_ID(); ?>">
+                    <?= esc_html(get_the_title()); ?>
+                    <span class="remove"><i class="fas fa-times"></i></span>
+                    </a>
+                </li>
+            
+                <?php endwhile; ?>
+            </ul>
+            <?php wp_reset_postdata(); ?>
+            <?php endif;
+        echo '
         </section>
     </div>
 </main>';
