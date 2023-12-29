ecfb3b047bc99dedf53d392e1c17b563ccd8b38e
= <=>
2023-12-29 12:02:37 -0600

    testing · css

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,47 +1,19 @@
-$('.filter-link').on('click', function(e) {
-    e.preventDefault();
-    $(this).toggleClass('activeFilter');
+jQuery(document).ready(function($) {
+    $('.category-list_item').on('click', function() {
+        $('.category-list_item').removeClass('active');
+        $(this).addClass('active');
 
-    editFilterInputs($('#filters-' + $(this).data('type')), $(this).data('id'));
-    filterProducts();
-  });
-
-
-
-  function editFilterInputs(inputField, value) {
-    const currentFilters = inputField.val().split(',');
-    const newFilter = value.toString();
-  
-    if (currentFilters.includes(newFilter)) {
-      const i = currentFilters.indexOf(newFilter);
-      currentFilters.splice(i, 1);
-      inputField.val(currentFilters);
-    } else {
-      inputField.val(inputField.val() + ',' + newFilter);
-    }
-  }
-
-
-
-  function filterProducts() {
-    const catIds = $('#filters-category').val().split(',');
-    const creatorIds = $('#filters-creators').val().split(',');
-    
-    $.ajax({
-      type: 'POST',
-      url: '/wp-admin/admin-ajax.php',
-      dataType: 'json',
-      data: {
-        action: 'filter_products',
-        catIds,
-        creatorIds,
-      },
-      success: function(res) {
-        $('#result-count').html(res.total);
-        $('#main-product-list').html(res.html);
-      },
-      error: function(err) {
-        console.error(err);
-      }
-    })
-  }
+        $.ajax({
+            type: 'PRODUCT',
+            url: '/wp-admin/admin-ajax.php',
+            dataType: 'html',
+            data: {
+                action: 'filter_projects',
+                category: $(this).data('slug'),
+            },
+            success: function(res) {
+                $('.products').html(res);
+            }
+        });
+    });
+});
