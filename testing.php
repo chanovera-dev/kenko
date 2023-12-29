25bc94c7dcf5172143c13b2457632514e64349c9
= <=>
2023-12-29 10:48:49 -0600

    agregado el template de testing para probar el filtrado de woocommerce

--- testing.php
+++ testing.php
@@ -5,22 +5,23 @@ echo '
 <main id="main">
     <div class="container">
         <section class="section breadcrumb-filter-search">
-            <div class="categories-list">';
+            <nav class="categories-list--wrapper">';
                 $taxonomy = 'product_cat';
                 $categories = get_terms(array(
                     'taxonomy'   => $taxonomy,
                     'hide_empty' => false,
                     'exclude'    => array(get_option('default_product_cat')), // Excluir la categoría 'sin categorizar'
                 ));
-                echo '<ul class="categories-list">
+                echo '
+                <ul class="categories-list">
                     <li><a class="category-list_item active" href="#!" data-slug="">' . esc_html__('Todo', 'kenko') . '</a></li>';
-                foreach($categories as $category) :
+                    foreach($categories as $category) :
                     echo '
                     <li><a class="category-list_item" href="#!" data-slug="' . $category->slug . '">' . $category->name . '</a></li>';
-                endforeach;
+                    endforeach;
                 echo '
                 </ul>
-            </div>
+            </nav>
         </section>
     </div>
     <div class="container">
