1506449e9a372c07d22257733af42290c117852d
= <=>
2023-12-29 17:41:08 -0600

    testing filters woo · css · php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -112,8 +112,13 @@ function my_custom_content_before_wrapper() {
     echo '<main id="main"><div class="container breadcrumb-wrapper">';
     // contenido de la parte superior de shop
     if ( is_shop() || is_product_category() || is_tax(get_object_taxonomies( 'product' )) ) {
-        echo '<section class="section breadcrumb-filter-search">';
-        if ( is_active_sidebar('shop-sidebar') ) { dynamic_sidebar('shop-sidebar'); }
+        echo '
+        <section class="section breadcrumb-filter-search">
+            <div class="filters">';
+                if ( is_active_sidebar('shop-sidebar') ) { dynamic_sidebar('shop-sidebar'); }
+            echo '
+            </div>';
+        
     }
     // contenido de la parte superior de single product
 	if ( is_product() ) {
