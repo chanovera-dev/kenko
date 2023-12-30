8713e5dedbee0b77f3a70a9dc9b1066f17067544
= <=>
2023-12-30 12:09:33 -0600

    probando filtro de woocommerce

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -116,6 +116,7 @@ function my_custom_content_before_wrapper() {
         <section class="section breadcrumb-filter-search">
             <div class="header-shop">' .
                 do_shortcode('[yith_wcan_filters slug="breadcrumb"]') . '
+                <button>Filtros</button>
             </div>
             <div class="filters">';
                 if ( is_active_sidebar('shop-sidebar') ) { dynamic_sidebar('shop-sidebar'); }
