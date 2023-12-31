90c3bdebe922cbaa356c8ddd8db4d57345183ae3
= <=>
2023-12-31 14:14:05 -0600

    botón para el panel de carrito

--- parts/sidebars/wishlist-panel.php
+++ parts/sidebars/wishlist-panel.php
@@ -1,4 +1,4 @@
 <div id="wishlist-panel">
     <button id="close-panels" class="close-panels" onclick="closePanels()"><?php echo esc_html__('Cerrar', 'kenko'); ?></button>
-    
+    <?php if ( is_active_sidebar('wishlist-sidebar') ) { dynamic_sidebar('wishlist-sidebar'); } ?>
 </div>
