245a19c23e7a40f91ed6fdffd64dbb8f51a894e5
= <=>
2023-12-31 13:59:52 -0600

    botón para el panel de carrito

--- parts/sidebars/cart-panel.php
+++ parts/sidebars/cart-panel.php
@@ -1,4 +1,4 @@
 <div id="cart-panel">
     <button id="close-panels" class="close-panels" onclick="closePanels()"><?php echo esc_html__('Cerrar', 'kenko'); ?></button>
-    
+    <?php if ( is_active_sidebar('mini-cart-sidebar') ) { dynamic_sidebar('mini-cart-sidebar'); } ?>
 </div>
