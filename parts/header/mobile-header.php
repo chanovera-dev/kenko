20fc7e6e3d2ead01d2732a97ccedd5a670b7e926
= <=>
2024-01-01 22:50:04 -0600

    correcciones par el menú primario en el heaader

--- parts/header/mobile-header.php
+++ parts/header/mobile-header.php
@@ -5,13 +5,18 @@ include(TEMPLATEPATH . '/parts/header/menu-mobile.php');
     echo '
     <section class="section header-content">';
         include(TEMPLATEPATH . '/parts/header/menu-button.php');
-        wp_nav_menu(
-            array(
-                'container' => 'nav', 
-                'container_class' => 'primary', 
-                'theme_location' => 'primary',
-            ) 
-        ); 
+        $menu_primary_id = get_nav_menu_locations()['primary'];
+        $menu_primary = wp_get_nav_menu_object($menu_primary_id);
+        $items_primary = wp_get_nav_menu_items($menu_primary_id);
+        if (!empty($items_primary)) {
+            wp_nav_menu(
+                array(
+                    'container' => 'nav',
+                    'container_class' => 'primary',
+                    'theme_location' => 'primary',
+                )
+            );
+        }
         include(TEMPLATEPATH . '/parts/header/brand.php');
         include(TEMPLATEPATH . '/parts/header/attachments.php');
     echo '
